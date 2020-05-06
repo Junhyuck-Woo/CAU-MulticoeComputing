4dd514e9bfe2f07cae90604d9b9688578dc4f0bd
Junhyuck-Woo <wjh0470@cau.ac.kr>
2020-05-06 17:07:55 +0900

    Upload example

---
+++ src/MyRunnable.java
@@ -0,0 +1,21 @@
+// Prof: Bongsoo Sohn
+// Org: College of Software, Chung-Ang University
+// Lecture: Multicore Computing
+// Title: MyRunnable.java
+// Date: May6, 2020
+
+class MyRunnable implements Runnable{
+    public void run() {
+        System.out.println("MyRunnable.run()");
+    }
+    //other methods and data for this class
+}
+
+class Main {
+    public static void main(String[] args) {
+        MyRunnable myrun = new MyRunnable();
+        Thread t1 = new Thread(myrun);
+        t1.start();
+        System.out.println("InsideMain()");
+    }
+}
