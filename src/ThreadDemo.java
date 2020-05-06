82b17f5b14d1c76098b7b3bc47a7d9a377ee01e2
Junhyuck-Woo <wjh0470@cau.ac.kr>
2020-05-06 16:53:37 +0900

    Add comments

--- src/ThreadDemo.java
+++ src/ThreadDemo.java
@@ -7,6 +7,7 @@
 class ThreadDemo {
     public static void main (String [] args) {
         MyThread mt = new MyThread ();
+        // mt thread and for-loop work simultaneously
         mt.start();
         for (int i = 0; i < 50; i++) {
             System.out.println("i = " + i + ", i * i = " + i * i);
